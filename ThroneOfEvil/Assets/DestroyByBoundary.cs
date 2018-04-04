d54630910d4963f43dc40ad026ed2edfd189d18a
Korrasami <tash123@live.co.za>
2018-04-04 10:31:33 +0200

    enemyspawn

---
+++ ThroneOfEvil/Assets/DestroyByBoundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
