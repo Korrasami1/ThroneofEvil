e0b335aa5a6b9bc220bb6b352eabcd88b5c03589
Korrasami <tash123@live.co.za>
2018-05-03 16:03:03 +0200

    the highscore board is working!!!

--- ThroneOfEvil/Assets/Scripts/Managers/HighScoreBoardManager.cs
+++ ThroneOfEvil/Assets/Scripts/Managers/HighScoreBoardManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using System.IO;
 
 public class HighScoreBoardManager : MonoBehaviour {
 	public Text[] scores;
@@ -11,25 +12,59 @@ public class HighScoreBoardManager : MonoBehaviour {
 	private string currentPlayerName;
 	List<PlayerScores> highscores;
 
-	// Use this for initialization
 	void Start () {
-		PlayerPrefs.SetInt ("HighScore", 50);
+		readfromFile ();
 		currentPlayerScore = PlayerPrefs.GetInt ("HighScore");
-		PlayerPrefs.SetString ("PlayerName", "steve");
 		currentPlayerName = PlayerPrefs.GetString ("PlayerName");
 		highscores = new List<PlayerScores> ();
+
 		//this adds the names with their scores to an array that so it can be sorted.
 		for (int i = 0; i < scores.Length; i++) {
 			highscores.Add (new PlayerScores () { names = Names [i].text, points = int.Parse (scores [i].text) });
 		}
 		highscores.Add (new PlayerScores () { names = currentPlayerName, points = currentPlayerScore });
+
 		//sorts and collects just the top 10 players
 		PlayerScores[] top10 = GetHighScores (10);
-
+	
 		//places the top 10 names into the scenes's text files
 		for (int i = 0; i < 10; i++) {
 			Names [i].text = top10 [i].names;
 			scores [i].text = top10 [i].points.ToString();
+		}
+		clearFiles ();
+		writeTofile ();
+		writeTofile2 ();
+	}
+
+	void writeTofile(){
+		for (int i = 0; i < 10; i++) {
+			StreamWriter write = new StreamWriter ("textfile.txt", true);
+			write.WriteLine (Names[i].text);
+			write.Close ();
+		}
+	}
+	void writeTofile2(){
+		for (int i = 0; i < 10; i++) {
+			StreamWriter write = new StreamWriter ("Scores.txt", true);
+			write.WriteLine (scores[i].text);
+			write.Close ();
+		}
+	}
+
+	void clearFiles(){
+		StreamWriter writer = new StreamWriter("textfile.txt", false);
+		StreamWriter writer2 = new StreamWriter("Scores.txt", false);
+		writer.Close();
+		writer2.Close ();
+	}
+
+	void readfromFile(){
+		string[] lines = System.IO.File.ReadAllLines("textfile.txt");
+		string[] lines2 = System.IO.File.ReadAllLines("Scores.txt");
+		for (int i = 0; i < Names.Length; i++) {
+			Names [i].text = lines[i];
+			scores [i].text = lines2[i];
 		}
 	}
 
---
+++ ThroneOfEvil/Assets/Scripts/NameInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour {
+	string[] alphabet;
+	public InputField l1, l2, l3, l4;
+	public GameObject n1, n2, n3, n4;
+	int AlphaNum = 0;
+	// Use this for initialization
+	void Start () {
+		alphabet = new string[27]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","-"};
+		n1.SetActive (false);
+		n2.SetActive (false);
+		n3.SetActive (false);
+		n4.SetActive (false);
+		gameObject.SetActive (true);
+	}
+		
+	public void changeLetterUp(InputField Alpha){
+		AlphaNum++;
+		if (AlphaNum < 0) {
+			AlphaNum = alphabet.Length-1;
+			Alpha.text = alphabet [AlphaNum];
+		} else if (AlphaNum >= alphabet.Length) {
+			AlphaNum = 0;
+			Alpha.text = alphabet [AlphaNum];
+		} else {
+			Alpha.text = alphabet [AlphaNum];
+		}
+	}
+	public void changeLetterDown(InputField Alpha){
+		AlphaNum--;
+		if (AlphaNum <= -1) {
+			AlphaNum = alphabet.Length-1;
+			Alpha.text = alphabet [AlphaNum];
+		} else if (AlphaNum >= alphabet.Length) {
+			AlphaNum = 0;
+			Alpha.text = alphabet [AlphaNum];
+		} else {
+			Alpha.text = alphabet [AlphaNum];
+		}
+	}
+
+	public void setName(){
+		string name = l1.text + l2.text + l3.text + l4.text;
+		PlayerPrefs.SetString ("PlayerName", name);
+		Debug.Log (name);
+		n1.SetActive (true);
+		n2.SetActive (true);
+		n3.SetActive (true);
+		n4.SetActive (true);
+		gameObject.SetActive (false);
+	}
+}
